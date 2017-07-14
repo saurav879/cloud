237e2208b06305d5512297e02957ab7a1c51a2dc
Saurav Tiwari <sauravtiwari7@gmail.com>
2017-07-14 14:29:38 +0530

    Add Some line in Index.php

--- index.php
+++ index.php
@@ -11,3 +11,17 @@ echo "<pre>";
 
 print_r(json_decode($new_data));
 ?>
+
+<?php
+$data = array(
+  'name' => 'jogn',
+  'age' => 21,
+  'adult' => 'yes'
+);
+
+$new_data = json_encode($data);
+
+echo "<pre>";
+
+print_r(json_decode($new_data));
+ ?>
